ea54dfcbfd440ce694fcfd308c4ed02d6db7ca20
gitcmartel <martel.c73@gmail.com>
2024-03-28 09:35:37 +0100

    Create ProductController.php
    Route getAllProducts and getProduct creation.

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/api/products', name: 'products', methods: ['GET'])]
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $productList = $productRepository->findAll();
+        $jsonProductList = $serializer->serialize($productList, 'json');
+        
+        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/product/{id}', name: 'product', methods: ['GET'])]
+    public function getProduct(Product $product, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonProduct = $serializer->serialize($product, 'json');
+        
+        return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
+        
+    }
+}
