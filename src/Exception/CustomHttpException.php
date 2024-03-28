6c356f620900c05393d3505de808a81433d9a0d2
gitcmartel <martel.c73@gmail.com>
2024-03-28 17:08:03 +0100

    Create CustomHttpException.php

---
+++ src/Exception/CustomHttpException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+class CustomHttpException extends HttpException
+{
+
+    public function __construct(int $statusCode, ConstraintViolationListInterface $errors, \Throwable $previous = null, array $headers = [], ?int $code = 0)
+    {
+        $detail = "Constraint violation error. ";
+        foreach ($errors as $error) {
+            // Affichage ou traitement de chaque erreur
+            $detail .= $error->getPropertyPath() . ' : ' . $error->getMessage() . ". ";
+        }
+        parent::__construct($statusCode, $detail, $previous, $headers, $code);
+    }
+}
