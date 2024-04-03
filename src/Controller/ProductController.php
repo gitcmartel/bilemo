0cc5b7f4382af9e539ca223792aa5f4fb607f13d
gitcmartel <martel.c73@gmail.com>
2024-04-03 19:57:15 +0200

    Adding IsGranted attribute

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,12 +7,14 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('/api/products', name: 'products', methods: ['GET'])]
+    #[IsGranted('ROLE_USER', message: 'Vous \'avez pas les droits suffisants pour obtenir la liste des produits')]
     public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $productList = $productRepository->findAll();
@@ -22,6 +24,7 @@ class ProductController extends AbstractController
     }
 
     #[Route('/api/product/{id}', name: 'product', methods: ['GET'])]
+    #[IsGranted('ROLE_USER', message: 'Vous \'avez pas les droits suffisants pour obtenir le détail d\'un produit')]
     public function getProduct(Product $product, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         if ($product === null) {
