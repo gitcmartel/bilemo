aeef0ded5cc46ec2b9afa776fe765bd3a810701a
gitcmartel <martel.c73@gmail.com>
2024-04-04 11:53:41 +0200

    Adding comments

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -15,9 +15,20 @@ use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
-
+    /**
+     * Endpoint to get the full products list
+     * 
+     * This method fetch all products from the database, serializes them in JSON format,
+     * and caches them to improve performance on subsequent queries.
+     * 
+     * @param ProductRepository $productRepository The product repository.
+     * @param SerializerInterface $serializer The serializer to convert objects to JSON.
+     * @param TagAwareCacheInterface $cache The cache to store temporary data.
+     * 
+     * @return JsonResponse The JSON response containing the list of products.
+     */
     #[Route('/api/products', name: 'products', methods: ['GET'])]
-    #[IsGranted('ROLE_USER', message: 'Vous \'avez pas les droits suffisants pour obtenir la liste des produits')]
+    #[IsGranted('ROLE_USER', message: 'You do not have sufficient rights to obtain the list of products')]
     public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
         $idCache = "getAllProducts";
@@ -32,8 +43,21 @@ class ProductController extends AbstractController
     }
 
 
+    /**
+    * Retrieve product details by its ID.
+    * 
+    * This method retrieves the details of a product identified by its ID.
+    * It checks if the user has the necessary permissions to view the product details,
+    * and returns a JSON response containing the serialized product information.
+    * 
+    * @param Product $product The product entity to retrieve.
+    * @param ProductRepository $productRepository The repository for product entities.
+    * @param SerializerInterface $serializer The serializer used to serialize the product object into JSON.
+    * 
+    * @return JsonResponse A JSON response containing the serialized product information.
+    */
     #[Route('/api/product/{id}', name: 'product', methods: ['GET'])]
-    #[IsGranted('ROLE_USER', message: 'Vous \'avez pas les droits suffisants pour obtenir le détail d\'un produit')]
+    #[IsGranted('ROLE_USER', message: 'You do not have sufficient rights to obtain the product details')]
     public function getProduct(Product $product, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         if ($product === null) {
