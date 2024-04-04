fa523b9bcaf0cd0b79f39b12de0e0a5631738db6
gitcmartel <martel.c73@gmail.com>
2024-04-04 11:27:16 +0200

    Adding cache system

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,18 +10,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
+
     #[Route('/api/products', name: 'products', methods: ['GET'])]
     #[IsGranted('ROLE_USER', message: 'Vous \'avez pas les droits suffisants pour obtenir la liste des produits')]
-    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
-        $productList = $productRepository->findAll();
-        $jsonProductList = $serializer->serialize($productList, 'json');
+        $idCache = "getAllProducts";
+
+        $jsonProductList = $cache->get($idCache, function (ItemInterface $item) use ($productRepository, $serializer) {
+            $item->tag("productsCache");
+            $productList = $productRepository->findAll();
+            return $serializer->serialize($productList, 'json');
+        });
         
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
+
 
     #[Route('/api/product/{id}', name: 'product', methods: ['GET'])]
     #[IsGranted('ROLE_USER', message: 'Vous \'avez pas les droits suffisants pour obtenir le détail d\'un produit')]
@@ -30,7 +39,7 @@ class ProductController extends AbstractController
         if ($product === null) {
             return new JsonResponse(null, Response::HTTP_NOT_FOUND);
         }
-        
+
         $jsonProduct = $serializer->serialize($product, 'json');
 
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
