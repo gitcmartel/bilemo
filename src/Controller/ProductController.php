9748b3a973482ab7a8422fcd2a2d854b641d7e80
gitcmartel <martel.c73@gmail.com>
2024-04-09 20:07:42 +0200

    Factoring functions parameters in class constructor

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -15,28 +15,42 @@ use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
+    private ProductRepository $productRepository;
+    private SerializerInterface $serializer;
+    private TagAwareCacheInterface $cache;
+    /**
+     * The class constructor
+     * 
+     * @param ProductRepository $productRepository The product repository.
+     * @param SerializerInterface $serializer The serializer to convert objects to JSON.
+     * @param TagAwareCacheInterface $cache The cache to store temporary data.
+     */
+    public function __construct(ProductRepository $productRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache) 
+    {
+        $this->productRepository = $productRepository;
+        $this->serializer = $serializer;
+        $this->cache = $cache;
+    }
+
     /**
      * Endpoint to get the full products list
      * 
      * This method fetch all products from the database, serializes them in JSON format,
      * and caches them to improve performance on subsequent queries.
      * 
-     * @param ProductRepository $productRepository The product repository.
-     * @param SerializerInterface $serializer The serializer to convert objects to JSON.
-     * @param TagAwareCacheInterface $cache The cache to store temporary data.
      * 
      * @return JsonResponse The JSON response containing the list of products.
      */
     #[Route('/api/products', name: 'getAllProducts', methods: ['GET'])]
     #[IsGranted('ROLE_USER', message: 'You do not have sufficient rights to obtain the list of products')]
-    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
+    public function getAllProducts(): JsonResponse
     {
-        $idCache = "getAllProducts";
+        $idCache = "productsCache";
 
-        $jsonProductList = $cache->get($idCache, function (ItemInterface $item) use ($productRepository, $serializer) {
+        $jsonProductList = $this->cache->get($idCache, function (ItemInterface $item) {
             $item->tag("productsCache");
-            $productList = $productRepository->findAll();
-            return $serializer->serialize($productList, 'json');
+            $productList = $this->productRepository->findAll();
+            return $this->serializer->serialize($productList, 'json');
         });
         
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
@@ -51,20 +65,18 @@ class ProductController extends AbstractController
     * and returns a JSON response containing the serialized product information.
     * 
     * @param Product $product The product entity to retrieve.
-    * @param ProductRepository $productRepository The repository for product entities.
-    * @param SerializerInterface $serializer The serializer used to serialize the product object into JSON.
     * 
     * @return JsonResponse A JSON response containing the serialized product information.
     */
     #[Route('/api/product/{id}', name: 'getProduct', methods: ['GET'])]
     #[IsGranted('ROLE_USER', message: 'You do not have sufficient rights to obtain the product details')]
-    public function getProduct(Product $product, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    public function getProduct(Product $product): JsonResponse
     {
         if ($product === null) {
             return new JsonResponse(null, Response::HTTP_NOT_FOUND);
         }
 
-        $jsonProduct = $serializer->serialize($product, 'json');
+        $jsonProduct = $this->serializer->serialize($product, 'json');
 
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
